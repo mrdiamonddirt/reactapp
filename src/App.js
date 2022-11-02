ca14acee3d6a81a731c8347fdcd84239d504a69b
RowDog <mrdiamonddirt@gmail.com>
2022-11-02 02:14:43 +0000

    added btns and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import styled from 'styled-components';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { useState } from "react";
+
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
+  const [openModal, setOpenModal] = useState(false)
+
+  const updateModal = () => {
+    setOpenModal(!openModal)
+  }
+  
+
   return (
-    <div className="App">
-    </div>
+ <>
+ <BrowserRouter>
+ <NavBar>
+  <NavItem to="/">Home</NavItem>
+  <NavItem to="/about">About</NavItem>
+  <NavItem to="/contact">Contact</NavItem>
+ </NavBar>
+ <Routes>
+  <Route path="/" element={ <Home changeModal={updateModal}/>}/>
+  <Route path="/about" element={ <About />}/>
+  <Route path="/contact" element={ <Contact />}/>
+ </Routes>
+ </BrowserRouter>
+ </>
   );
 }
 
+
 export default App;
+
+const NavBar = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: lightblue;
+    padding: 20px;
+`;
+
+const NavItem = styled(Link)`
+    font-size: 16px;
+    margin: 0 16px;
+    color: white;
+    cursor: pointer;
+    padding: 10px 15px;
+    
+    &:hover{
+        background-color: aqua;
+    }
+`;
